a90bc8f69e263217c2d45796a0e2db69ac7b51e4
angel <angeledugo@hotmail.com>
2013-08-28 16:27:48 -0430

    se realizan cambios en el layout principal

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -93,7 +93,7 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/trabajo.phtml',
             'layout/admin'            => __DIR__ . '/../view/layout/admin.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
--- module/Application/src/Application/Form/Formularios.php
+++ module/Application/src/Application/Form/Formularios.php
@@ -10,10 +10,10 @@ use Zend\Form\Factory;
 class Formularios extends Form
 {
     public function __construct($name = null, $options = array()) {
-        
+
         parent::__construct($name);
-        $factory = new Factory();  
-        
+        $factory = new Factory();
+
         $correo = $factory->createElement(array(
             'type'=>'Zend\Form\Element\Email',
             'name'=>'email',
@@ -24,19 +24,19 @@ class Formularios extends Form
                 'class' => 'input',
                 'required'=>'required',
                 'pattern'=>"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$"
-                
-            ),  
+
+            ),
         ));
-        
+
         $this->add($correo);
-        
+
         $add = $factory->createElement(array(
            'name'=>'send',
            'attributes'=>array(
               'type'=>'submit',
                 'value'=>'Suscribirse',
                 'title'=>'Suscribirse',
-                'class'=>'btn-blue' 
+                'class'=>'btn btn-danger'
             ),
         ));
         $this->add($add);

4dd98410470795518cfc1a90c31ecd7b19f1c392
angel <angeledugo@hotmail.com>
2013-08-29 16:28:50 -0430

    se continuan los cambios en el diseno de la pagina

--- public/sofvecom_sofve.sql
+++ public/sofvecom_sofve.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50527
 File Encoding         : 65001
 
-Date: 2013-08-26 16:15:35
+Date: 2013-08-29 15:34:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -67,18 +67,22 @@ CREATE TABLE `sofve_post` (
   `id_usuarios` int(11) DEFAULT NULL,
   `id_category` int(11) DEFAULT NULL,
   `img_post` varchar(50) DEFAULT NULL,
+  `texto_post` text,
+  `video_post` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id_post`),
   KEY `usuarios_posts` (`id_usuarios`),
   KEY `category_post` (`id_category`),
   CONSTRAINT `category_post` FOREIGN KEY (`id_category`) REFERENCES `sofve_category` (`id_category`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `usuarios_posts` FOREIGN KEY (`id_usuarios`) REFERENCES `sofve_usuarios` (`id_usuarios`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of sofve_post
 -- ----------------------------
-INSERT INTO `sofve_post` VALUES ('1', 'Sofve: DiseÑo Web y Servicio a Sus Clientes', 'Bienvenida  al sistema', '0000-00-00', '0', '2013-08-26 16:13:44', '1', '1', '/img/pagina_peq.JPG');
-INSERT INTO `sofve_post` VALUES ('2', 'Zend Framework: Primeros Pasos', 'Damos Inicio a este primer tutorial relacionado al desarrollo web y en esta ocacion al uso de un framework gratutito para el desarrollo de aplicaciones en PHP', '2013-08-26', '0', '2013-08-26 16:14:07', '1', '2', '/img/zend-logo.PNG');
+INSERT INTO `sofve_post` VALUES ('1', 'Sofve: DiseÑo Web y Servicio a Sus Clientes', 'Bienvenida  al sistema', '0000-00-00', '0', '2013-08-26 16:13:44', '1', '1', '/img/pagina_peq.JPG', null, null);
+INSERT INTO `sofve_post` VALUES ('2', 'Zend Framework: Primeros Pasos', 'Damos Inicio a este primer tutorial relacionado al desarrollo web y en esta ocacion al uso de un framework gratutito para el desarrollo de aplicaciones en PHP', '2013-08-26', '0', '2013-08-27 14:49:55', '1', '2', '/img/zend-logo.PNG', 'Damos Inicio al Primer Tutorial relacionado al Zend Framework para el desarrollo de Aplicaciones en Php a continuacion mencionaremos las diferentes formas para el proceso de Instalacion y como iniciar nuestro primer proyecto. <br> - Instalacion Usando Git <br> - Instalacion Usando Composer <br>-  Instalacion a Traves de Zend Tool   ', null);
+INSERT INTO `sofve_post` VALUES ('3', 'Disponible la Nueva Version de Boostrap 3', 'Ya se encuentra disponible la nueva version de este popular framework para el desarrollo de aplicaciones Web', '2013-08-29', '0', '2013-08-29 15:08:40', '1', '2', '/img/boostrap.png', 'A continuacion mencionamos las nuevas caracteristicas que trae el nuevo Boostrap 3 <br> - Nuevo diseño y la Posibilidad de un Tema Opcional <br> - El Primero en El desarrollo Responsive para Aplicaciones Mobile <br> - Nuevo Personalizador <br> - Mejor modelo de caja de forma predeterminada. <br> - Sistema de Grid Mejorado <br> - Mejoras en los Pluggins Javascripts <br> - Nueva fuentes Glyphicons. <br> - Nueva barra de Navegación Responsive. <br> - Los Modelos son Mucho mas Sensibles <br> - Adicionado Nuevos Componentes <br> - Removido antiguos Componentes <br> - Mejor Consistencia para el Dimensionamiento de las Clases. <br> - Documentación Actualizada <br> - Eliminado el Soporte para internet explorer 7 y Firefox 3.6', null);
+INSERT INTO `sofve_post` VALUES ('4', 'Nueva Imagen Para las conversaciones En Twitter', 'Twitter anucio el lanzamiento de una nueva imagen con el que pretende ofrecer una mejora significativa a sus Usuarios', '2013-08-29', '0', '2013-08-29 15:30:17', '1', '2', '/img/twiiter.gif', 'La Plataforma de Comunicacion dio a conocer un cambio significativo donde los usuarios podran seguir las conversaciones de una forma mas sencilla estos cambios afectaran tanto a Twitter.com como a las apps moviles para Android e IOS <br> A continuacion Podras Observar el Video de las Nuevas Caracteristicas de Twitter para sus usuarios', null);
 
 -- ----------------------------
 -- Table structure for sofve_usuarios
